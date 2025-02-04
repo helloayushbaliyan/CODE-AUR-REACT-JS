18a4b175e820a64b9487739ecda8d37839072c1a
Ayush Baliyan <helloayushbaliyan@gmail.com>
2025-02-04 13:30:35 +0530

    know how to use props in react js

--- 03-tailwind-props/src/App.jsx
+++ 03-tailwind-props/src/App.jsx
@@ -4,15 +4,23 @@ import Card from './components/card'
 function App() {
   const [count, setCount] = useState(0)
 
+  let myobj = {
+    username:"Ayush Baliyan",
+    btntxt:"click me"
+  }
+  let otherobj = {
+    username:"vikas jawla",
+    
+
+  }
   return (
     <>
-     
+
       <div className=" grid h-screen w-full place-content-center ">
 
         <div className="flex gap-5">
-          <Card />
-          <Card />
-          
+          <Card name={myobj.username} btntext={myobj.btntxt}/>
+          <Card name={otherobj.username} btntext={otherobj.btntxt} />
 
         </div>
       </div>
