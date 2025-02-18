899fade11f3c32d4f138520f02e59eac5dcbc963
Ayush Baliyan <helloayushbaliyan@gmail.com>
2025-02-18 22:15:43 +0530

    lest start working with context api

---
+++ 08-mini-context/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   <div className="bg-slate-900 h-screen">
+    <h1 className='text-white text-5xl text-center p-20 capitalize'>context api</h1>
+    </div></>
+  )
+}
+
+export default App
